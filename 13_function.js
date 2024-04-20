b17dedf52f86255cd5cc54c6d1095f99c8fb507b
sakib khan <kshaquib17@gmail.com>
2024-04-20 15:07:14 +0530

    commit

---
+++ 13_function.js
@@ -0,0 +1,23 @@
+// function 
+
+function sayMyName() {
+
+ console.log("s");
+console.log("h");
+console.log("a");
+console.log("q");
+console.log("u");
+console.log("i");
+console.log("b");
+    
+}
+
+// sayMyName()
+
+function addTwoNumbers(number1, number2)
+{
+    console.log(number1 + number2);
+}
+
+addTwoNumbers(499,765)      // call the function 
+
