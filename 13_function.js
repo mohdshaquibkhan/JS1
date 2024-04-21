1af562c85c71f8bd5ec7627620a63bac1147ef25
sakib khan <kshaquib17@gmail.com>
2024-04-21 12:44:48 +0530

    2 commit

--- 13_function.js
+++ 13_function.js
@@ -16,8 +16,29 @@ console.log("b");
 
 function addTwoNumbers(number1, number2)
 {
-    console.log(number1 + number2);
+  //  let result = number1 + number2
+   // console.log("haniya");
+   // return result
+
+   return number1 + number2
+
 }
 
 addTwoNumbers(499,765)      // call the function 
+
+const result = addTwoNumbers(3, 5)
+
+ // console.log("Result: ", result);
+
+
+ function loginUserMessage(username){
+    if(username === undefined){
+        console.log("Please enter a username");
+        return
+
+    }
+    return `${username} just logged in`
+ }
+
+ console.log(loginUserMessage())
 
